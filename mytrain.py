43125e82257df45f0665cdc1f04380cbaa5df260
Uniaut <kunwoo0927@gmail.com>
2023-11-24 16:43:56 +0900

    Update validation set transformation in mytrain.py

--- mytrain.py
+++ mytrain.py
@@ -139,9 +139,16 @@ def train(opt):
     )
     training_generator = DataLoader(training_set, **training_params)
 
-    val_set = CocoDataset(root_dir=os.path.join(opt.data_path, params.project_name), set=params.val_set,
-                          transform=transforms.Compose([Normalizer(mean=params.mean, std=params.std),
-                                                        Resizer(input_sizes[opt.compound_coef])]))
+    val_transform = A.Compose([
+        A.LongestMaxSize(max_size=input_size),
+        A.Normalize(mean=params.mean, std=params.std),
+        A.PadIfNeeded(min_height=input_size, min_width=input_size, position=A.PadIfNeeded.PositionType.TOP_LEFT)
+    ], bbox_params=A.BboxParams(format='coco', label_fields=['category_id']))
+    val_set = CocoDatasetForAlbumentations(
+        root_dir=os.path.join(opt.data_path, params.project_name),
+        set=params.val_set,
+        transform=val_transform
+    )
     val_generator = DataLoader(val_set, **val_params)
 
     model = EfficientDetBackbone(num_classes=len(params.obj_list), compound_coef=opt.compound_coef,
